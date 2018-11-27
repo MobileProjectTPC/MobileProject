fc853f09f738a18cb20e938a63805c00cef95e46
HuiG33 <joni.tefke@metropolia.fi>
2018-11-27 11:47:45 +0200

    functionality to update user password, and have to insert current password before updating any data

--- app/src/main/java/com/example/joni/mobileproject/fragments/UpdateUserDataFragment.kt
+++ app/src/main/java/com/example/joni/mobileproject/fragments/UpdateUserDataFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.example.joni.mobileproject.R
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.update_user_data_fragment_layout.*
@@ -29,10 +30,7 @@ class UpdateUserDataFragment: Fragment() {
         Log.d("UpdateUserDataFragment", "UpdateUserDataFragment created")
 
         btnUpdateData.setOnClickListener {
-            if (!TextUtils.isEmpty(edtUpdateFirst.text) || !TextUtils.isEmpty(edtUpdateLast.text)){
-                updateData(firebaseAuth.uid!!, edtUpdateFirst.text.toString(), edtUpdateLast.text.toString())
-            }
-            updateEmail(firebaseAuth.uid!!, edtUpdateEmail.text.toString(), edtConfirmUpdateEmail.text.toString())
+            reAuthenticate(edtOldPass.text.toString())
         }
 
         btnCancel.setOnClickListener {
@@ -74,6 +72,42 @@ class UpdateUserDataFragment: Fragment() {
             else {
                 Toast.makeText(context, "Emails does not match", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun updatePassword(newPassword: String, confirmPassword: String){
+        if (!TextUtils.isEmpty(newPassword) || !TextUtils.isEmpty(confirmPassword)){
+            if (TextUtils.equals(newPassword, confirmPassword)){
+                firebaseAuth.currentUser!!.updatePassword(newPassword)
+                        .addOnSuccessListener {
+                            Log.d("UpdateUserDataFragment", "Password updated!")
+                        }
+                        .addOnFailureListener {
+                            Log.d("UpdateUserDataFragment", "Foggening $it")
+                        }
+            }
+            else{
+                Toast.makeText(context, "Passwords does not match", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun reAuthenticate(password: String){
+        if (!TextUtils.isEmpty(password)){
+            val credentials = EmailAuthProvider.getCredential(firebaseAuth.currentUser!!.email!!, password)
+            firebaseAuth.currentUser!!.reauthenticate(credentials)
+                    .addOnSuccessListener {
+                        updateData(firebaseAuth.uid!!, edtUpdateFirst.text.toString(), edtUpdateLast.text.toString())
+                        updateEmail(firebaseAuth.uid!!, edtUpdateEmail.text.toString(), edtConfirmUpdateEmail.text.toString())
+                        updatePassword(edtUpdatePass.text.toString(), edtConfirmUpdatePass.text.toString())
+                    }
+                    .addOnFailureListener {
+                        Log.d("UpdateUserDataFragment", "Foggening $it")
+                        Toast.makeText(context, "Authentication failed", Toast.LENGTH_SHORT).show()
+                    }
+        }
+        else{
+            Toast.makeText(context, "Enter password", Toast.LENGTH_SHORT).show()
         }
     }
 }
